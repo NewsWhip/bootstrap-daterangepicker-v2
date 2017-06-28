df406869758e45cb185b0ee57379852f75f4ea37
codeepic <ksaweryglab@gmail.com>
2017-06-28 12:48:12 +0100

    update version info

--- daterangepicker.js
+++ daterangepicker.js
@@ -1,5 +1,5 @@
 /**
- * @version: 1.0.2
+ * @version: 1.0.4
  * @author: Xavier Glab http://github.com/codeepic based on Dan Grossman's http://www.dangrossman.info/ package
  * @copyright: Copyright (c) 2012-2015 Dan Grossman. All rights reserved.
  * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
@@ -1392,8 +1392,7 @@
                     this.clickApply();
             }
 
-            const onDateClick = true;
-            this.updateView(onDateClick);
+            this.updateView(true);
         },
 
         calculateChosenLabel: function() {
