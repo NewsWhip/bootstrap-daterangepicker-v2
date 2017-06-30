ad9c5c079aff649eb7ed882ecf4ee65be93dbcef
codeepic <ksaweryglab@gmail.com>
2017-06-30 13:29:41 +0100

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nwa-daterangepicker',
-  version: '1.0.7',
+  version: '1.0.8',
   summary: 'Daterangepicker based on Dan Grossman\'s bootstrap-daterangepicker.',
   git: 'https://github.com/NewsWhip/nwa-daterangepicker.git',
   documentation: 'README.md'
