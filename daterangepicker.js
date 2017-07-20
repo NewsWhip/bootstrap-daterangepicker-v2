ce7fc05f7f7be912114e1a8b235b02b51dd38fe4
codeepic <ksaweryglab@gmail.com>
2017-07-20 11:39:55 +0100

    check why updated package is not pulled into nwa project

--- daterangepicker.js
+++ daterangepicker.js
@@ -1,5 +1,5 @@
 /**
- * @version: 1.0.20
+ * @version: 1.0.21
  * @author: Xavier Glab http://github.com/codeepic based on Dan Grossman's http://www.dangrossman.info/ package
  * @copyright: Copyright (c) 2012-2015 Dan Grossman. All rights reserved.
  * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
@@ -1083,6 +1083,7 @@
 
         },
         updateFormInputs: function() {
+            console.log('%c updateFormInputs fn RUNNING', 'background: purple; border: 2px solid black');
             //ignore mouse movements while an above-calendar text input has focus
             if (this.$startDateInput.is(":focus") || this.$endDateInput.is(":focus"))
                 return;
